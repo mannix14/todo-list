4bc978bea10fae188c144a88faf4888506978584
Mannix Protusada <mannix143@gmail.com>
2023-07-30 20:36:32 -0400

    completed tofolist

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const formEl = document.querySelector(".form")
 const inputEl = document.querySelector(".input")
 const ulEl = document.querySelector(".list")
 
+//convert list to array
+let list = JSON.parse(localStorage.getItem("list"))
+
+list.forEach(task => {
+  toDoList(task)
+})
 
 formEl.addEventListener("submit", (event) =>
 {
@@ -11,9 +17,21 @@ formEl.addEventListener("submit", (event) =>
 }
 );
 
-function toDoList() {
+function toDoList(task) {
   let newTask = inputEl.value;
+  if (task) {
+    newTask = task.name
+  }
+
+
+
   const liEl = document.createElement("li");
+
+  //this is when refreshed, the unchecked(grayed out) is still there
+  if (task && task.checked) {
+    liEl.classList.add("checked")
+}
+
   liEl.innerText = newTask;
   ulEl.appendChild(liEl);
   inputEl.value = "";
@@ -28,11 +46,27 @@ function toDoList() {
 
   checkBtnEl.addEventListener("click", () => {
     liEl.classList.toggle("checked")
+    updateLocalStorage()
   });
 
   trashBtnEl.addEventListener("click", () => {
     liEl.remove();
+    updateLocalStorage()
   });
+  updateLocalStorage()
 
 }
+
+function updateLocalStorage() {
+  const liEls = document.querySelectorAll("li")
+  let list = []
+  liEls.forEach(liEl => {
+    list.push({
+      name: liEl.innerText,
+      checked: liEl.classList.contains("checked")
+    })
+  })
+  localStorage.setItem("list", JSON.stringify(list))
+}
+
 
